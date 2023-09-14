889b83529800fec77ef33903497b4b535472598b
aakash <aakashruhal99@gmail.com>
2023-09-14 23:42:19 +0530

    Fix Menu on Mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
           window.removeEventListener('scroll', handleScroll);
         };
         }, []);
-      const headerClass = isSticky ? 'position-absolute w-100 sticky' : 'position-absolute w-100';
+        const StickyheaderClass = isSticky ? 'navbar navbar-expand-lg center-nav transparent navbar-light navbar-clone fixed navbar-stick' : 'navbar navbar-expand-lg center-nav transparent navbar-light navbar-clone fixed navbar-unstick';
       const topheaderClass = isSticky ? 'gradient-5 text-white fw-bold fs-15 mb-2 position-relative d-none' : 'gradient-5 text-white fw-bold fs-15 mb-2 position-relative';
 
 
@@ -45,8 +45,102 @@ const Header = () => {
       
 
   return (
+    <>
+    <nav className={StickyheaderClass}>
+        <div className="container flex-lg-row flex-nowrap align-items-center">
+          <div className="navbar-brand w-100">
+            <Link href="/">           
+                <Image
+                  src="/images/logo-dark.png"   
+                  alt="logo"
+                  width={134} 
+                  height={26} 
+                />
+            </Link>
+          </div>
+          <div className={`navbar-collapse offcanvas offcanvas-nav offcanvas-start ${isOffcanvasVisible ? 'show' : ''}`}>
+            <div className="offcanvas-header d-lg-none">
+              <h3 className="text-white fs-30 mb-0">Sandbox</h3>
+              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"
+                onClick={hideOffcanvas}>                    
+              </button>
+            </div>
+            <div className="offcanvas-body ms-lg-auto d-flex flex-column h-100">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#" data-bs-toggle="dropdown">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" data-bs-toggle="dropdown">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                <a className={`nav-link dropdown-toggle ${isDropdownVisible ? 'show' : ''}`}
+                 onClick={toggleDropdown}>
+                 Services
+                </a>
+                 <ul className={`dropdown-menu ${isDropdownVisible ? 'show' : ''}`}>
+                    <li className="nav-item">                      
+                      <a className="dropdown-item" href="#" data-bs-toggle="dropdown">Demo</a>
+                    </li>                    
+                    <li className="nav-item">
+                    <a className="dropdown-item" href="#" data-bs-toggle="dropdown">Demos</a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" data-bs-toggle="dropdown">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <div className="offcanvas-footer d-lg-none">
+                <div>
+                  <a href="mailto:first.last@email.com" className="link-inverse">
+                    info@email.com
+                  </a>
+                  <br />00 (123) 456 78 90
+                  <br />
+                  <nav className="nav social social-white mt-4">
+                  <a href="#"><i className="fa-brands fa-facebook-f"></i></a>
+                  <a href="#"><i className="fa-brands fa-x-twitter"></i></a>
+                  <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                  <a href="#"><i className="fa-brands fa-youtube"></i></a>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="navbar-other w-100 d-flex ms-auto">
+            <ul className="navbar-nav flex-row align-items-center ms-auto">
+              <li className="nav-item d-none d-lg-block">
+                <nav className="nav social social-muted justify-content-end text-end">
+                <a href="#"><i className="fa-brands fa-facebook-f"></i></a>
+                  <a href="#"><i className="fa-brands fa-x-twitter"></i></a>
+                  <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                  <a href="#"><i className="fa-brands fa-youtube"></i></a>
+                </nav>
+              </li>
+              <li className="nav-item d-lg-none">
+              <button className={`hamburger offcanvas-nav-btn ${isOffcanvasVisible ? 'show' : ''}`}
+                 onClick={toggleOffcanvas}>
+                  <span></span>
+              </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+
+
+
     
-    <header className={headerClass}>
+    <header className="position-absolute w-100">
       <div className={topheaderClass} style={{ zIndex: 1 }}>
       <div className="container py-2 text-center">
         <p className="mb-0">✨ Sandbox is part of Envato{"'"}s Biggest Sale to date, be quick and grab a </p>
@@ -141,6 +235,7 @@ const Header = () => {
         </div>
       </nav>
     </header>
+    </>
   )
 }
 
